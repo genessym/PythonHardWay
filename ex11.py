816fe33b943a5fd3715f0c401529a92048e71102
Genessy Munoz <genessy@Genessys-MBP.fios-router.home>
2017-06-28 23:13:21 -0400

    Exercise 11

---
+++ ex11.py
@@ -0,0 +1,16 @@
+print "How old are you?",
+age = raw_input()
+
+print "How tall are you",
+height = raw_input()
+
+print "How much do you weigh?",
+weight = raw_input()
+
+print "So, you're %r old, %r tall and %r heavy." %(age, height, weight)
+
+#Study Drills:
+my_food = raw_input("What is your favorite food?")
+print "%s is so delicious" % my_food
+
+x = int(raw_input())
