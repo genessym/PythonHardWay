d8af2291538e764e636c65265fec91c5b9443797
Genessy Munoz <genessy@Genessys-MBP.fios-router.home>
2017-07-12 10:25:17 -0400

    Fixed some spelling errors

--- ex36.py
+++ ex36.py
@@ -1,5 +1,5 @@
 from sys import exit
-print("\nAs you start to gain back consciousness, you start to feel someone shaking you.")
+print("\nAs you start to gain back consciousness, you feel someone shaking you.")
 print ("""\ney.... Hey! Wake up!
 Who are you?""")
 name = input("> ")
@@ -11,8 +11,8 @@ if location == "yes":
     dead("\nReally? Well I guess you don't need my help then. Good luck!")
 else: 
     print("\nWell allow me to introduce myself, the names Bob! And I am the host of this interesting place.")
-    print("""While Im sure you are dying to know why you are here, you probably should know where you are first. Well {}, you are at Simulator Model X Corp.""".format(name))
-    print("""\nToday, you were chosen as our lucky contestant to try out our latest stimulator device. While confusing at first, it is actually quite simple. You will enter the door before you and will have the option of selecting 1 of the four doors. Each door leads to a real life world simulator where you will be faced with choices that could lead to eternal happiness or your impending doom.""")
+    print("""While I'm sure you are dying to know why you are here, you probably should know where you are first. Well {}, you are at Simulator Model X Corp.""".format(name))
+    print("""\nToday, you were chosen as our lucky contestant to try out our latest stimulator device. While confusing at first, it is actually quite simple. You will enter the door before you and will have the option of selecting one of the three doors. Each door leads to a real life world simulator where you will be faced with choices that could lead to eternal happiness or your impending doom.""")
     print("And the best part is, its actually all real! Sounds fun, right!?")
     print("\n.......Well Come on! Walk right on in!")
 
@@ -25,7 +25,7 @@ def dead(why):
     exit(0)
 
 def beginning():
-    print("\nYou enter a white room, before you stands 4 doors.")
+    print("\nYou enter a white room, before you stands 3 doors.")
     print("Which one do you take?")
     print("1. Door One")
     print("2. Door Two")
@@ -55,9 +55,9 @@ def horror_simulator():
     elif next == "2":
         horror_simulator2()
     elif next == "3":
-        print ("A floating head trips you and you die from a conussion.")
+        print ("A floating head trips you and you die from a concussion.")
     else:
-        dead("You hear someone walking down the stairs. As you peer your head around the corner, you see Will Smith. He takes your hand and teleports you to Hawaii and live the rest of your days in peace.")
+        dead("You hear someone walking down the stairs. As you peer your head around the corner, you see Will Smith. He opens a teleportation hole and you escape.")
 
 def horror_simulator2():
     print ("\nAs you reach the first floor, you hear a baby crying. It sounds like it's coming from the left.")
@@ -68,14 +68,14 @@ def horror_simulator2():
     if decide == "1":
         print("There's a 5 ft baby sitting on a yoga ball. He gets up and carries you bridal style. You both walk out the hospital and walk into the sunset.")
     elif decide == "2":
-        print("A 20 ft tall tarantula chips off your head and feeds your body to it's babies")
+        print("A 20 ft tall tarantula rips off your head.")
     else:
         dead("A 10 ft tall clown chokes you to death from behind.")
 
 def office_simulator():
     print("Phone rings. You suddenly wake up in a office cubicle at your everyday average paper export company.")
-    print("You get up to make some coffees")
-    print("As you reach the copy room, you see fat Marge your cubicle neighbor and Dan the copy guy.")
+    print("You get up to make some copies.")
+    print("As you reach the copy room, you see chubby Marge your cubicle neighbor, Dan the copy guy and Mark your boss.")
     print("1. Marge")
     print("2. Dan")
     print("3. Mark")
@@ -100,9 +100,9 @@ def marge():
     if decide == "1":
         print("Fast forward a month later, you are engaged to Marge and your picking out wedding decor colors.")
     elif decide == "2":
-        print("Marge throws coffee in your face and your suit gets a stain. Leading to you dying from life long depression of never getting that coffee stain out.")
+        print("Marge throws coffee in your face and your suit gets a stain. You develop life long depression of never getting that coffee stain out.")
     else:
-        dead("She becomes your stalker and kills your in your sleep at the age of 35.")
+        dead("She awkwardly turns around. Later that night, Marge kills you in your sleep.")
 
 def dan():
     print ("What do you say?")
@@ -121,21 +121,31 @@ def dan():
 
 def game_simulator():
     print("You wake up in the middle of a battle against Lucas (from Super Smash Bros)")
-    print("Along the side, you see two figures who will fight for you: ")
+    print("Along your side, you see three figures willing to fight for you: ")
     print("1. Phoenix Wright")
     print("2. Cooking Mama")
+    print("3. Wii Fit Trainer")
     print("Who do you choose?")
     decide = input("> ")
     if decide == "1":
-        print("\"OBJECTION\" phoenix shouts. You both get obliterated by pk thunder.")
+        print("\"OBJECTION\" phoenix shouts....... You both get obliterated by pk thunder.")
     elif decide == "2":
-        print("As cooking mama begins to whisk with her all might. You both get obliterated by pk freeze.")
+        print("As cooking mama begins to whisk with her all might........ You both get obliterated by pk freeze.")
+    elif decide == "3":
+        print ("It looks like Lucas has met his match. They fight till the bitter end......")
+        print("When suddenly a voice from the heavens grumbles \"Sudden Death\".")
+        print("With Lucas distracted, wii fit trainer obliterates Lucas with sun salutation.")
     else:
-        dead("You flee and surprisingly... you get away.")
+        dead("As you look around frantically, Lucas decides to take mercy on your life and lets you escape.... As you begin to run north, you end up bumping into Kirby. He eat's you.")
 
 
+def while_loop():
+    print("Press enter to begin:")
+    enter = input("> ")
+    while enter != "": 
+        print("Not sure what that means")
+        enter = input("> ")
+    beginning()
 
-
-beginning()
-
+while_loop()
 
