66b2f15fc3ecfb22b64f73f298d2dab3ee56139d
Genessy Munoz <genessy@Genessys-MBP.fios-router.home>
2017-07-12 23:00:09 -0400

    Exercise 37

---
+++ ex37.py
@@ -0,0 +1,66 @@
+# imports pygame (free/open source library used for making multimedia applications)
+import pygame
+# imports random function
+import random
+
+# define some colors (R, G, B)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+BLACK = (0, 0, 0)       
+FUCHSIA = (255, 0, 255)
+GRAY = (128, 128, 128)
+LIME = (0, 128, 0)
+MAROON = (128, 0, 0)
+NAVYBLUE = (0, 0, 128)
+OLIVE = (128, 128, 0)
+PURPLE = (128, 0, 128)
+RED = (255, 0, 0)
+SILVER = (192, 192, 192)
+TEAL = (0, 128, 128)
+YELLOW = (255, 255, 0)
+ORANGE = (255, 128, 0)
+CYAN = (0, 255, 255)
+
+# basic constants to set up your game
+WIDTH = 360
+HEIGHT = 480
+FPS = 30
+BGCOLOR = BLACK
+
+# initialize pygame
+pygame.init()
+# initialize sound - uncomment if you're using sound
+# pygame.mixer.init()
+# create the game window and set the title
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("My Game")
+# start the clock
+clock = pygame.time.Clock()
+
+# set the 'running' variable to False to end the game
+running = True
+# start the game loop
+while running:
+    # keep the loop running at the right speed
+    # Not sure what FPS is
+    clock.tick(FPS)
+    # Game loop part 1: Events #####
+    for event in pygame.event.get():
+        # this one checks for the window being closed
+        if event.type == pygame.QUIT:
+            pygame.quit()
+        # add any other events here (keys, mouse, etc.)
+
+    # Game loop part 2: Updates #####
+
+    # Game loop part 3: Draw #####
+    screen.fill(BGCOLOR)
+    # after drawing, flip the display
+    pygame.display.flip()
+
+# close the window
+pygame.quit()
+
+#Study Drills:
+# Flow chats are basically code in visual form. It shows you the steps your code takes till the end.
